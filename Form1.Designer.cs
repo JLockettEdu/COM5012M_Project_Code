965c179c862b0de48ee6054d2de927c009106d4a
JLockett1403 <148762222+JLockettEdu@users.noreply.github.com>
2024-01-13 19:25:04 +0000

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,299 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.D1 = new System.Windows.Forms.DataGridView();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.comboBoxOrderBy = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.D1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(535, 24);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(110, 49);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "View Data";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // D1
+            // 
+            this.D1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.D1.Location = new System.Drawing.Point(17, 91);
+            this.D1.Margin = new System.Windows.Forms.Padding(2);
+            this.D1.Name = "D1";
+            this.D1.RowHeadersWidth = 51;
+            this.D1.RowTemplate.Height = 24;
+            this.D1.Size = new System.Drawing.Size(978, 457);
+            this.D1.TabIndex = 1;
+            this.D1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(775, 24);
+            this.button2.Margin = new System.Windows.Forms.Padding(2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 49);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Insert Data";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(86, 10);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(86, 32);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 4;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(86, 55);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 5;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(246, 10);
+            this.textBox4.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 6;
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(246, 32);
+            this.textBox5.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 20);
+            this.textBox5.TabIndex = 7;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(246, 55);
+            this.textBox6.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(100, 20);
+            this.textBox6.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 12);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "ClientID";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(26, 37);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "First Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(26, 60);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Last Name";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(193, 15);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(52, 13);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Postcode";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(202, 37);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(38, 13);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Phone";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(202, 60);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(32, 13);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "Email";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(372, 37);
+            this.label7.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(49, 13);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "Category";
+            this.label7.Click += new System.EventHandler(this.label7_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Software",
+            "Laptops & PC",
+            "Games",
+            "Office Tools",
+            "Accessories"});
+            this.comboBox1.Location = new System.Drawing.Point(423, 35);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(92, 21);
+            this.comboBox1.TabIndex = 17;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(660, 24);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(110, 49);
+            this.button4.TabIndex = 18;
+            this.button4.Text = "Edit Data";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // comboBoxOrderBy
+            // 
+            this.comboBoxOrderBy.FormattingEnabled = true;
+            this.comboBoxOrderBy.Location = new System.Drawing.Point(891, 24);
+            this.comboBoxOrderBy.Name = "comboBoxOrderBy";
+            this.comboBoxOrderBy.Size = new System.Drawing.Size(104, 21);
+            this.comboBoxOrderBy.TabIndex = 19;
+            this.comboBoxOrderBy.SelectedIndexChanged += new System.EventHandler(this.comboBoxOrderBy_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1014, 564);
+            this.Controls.Add(this.comboBoxOrderBy);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.D1);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.D1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView D1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.ComboBox comboBoxOrderBy;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+using Org.BouncyCastle.Crypto.Parameters;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public object DataGridViewStd { get; private set; }
+        private Database myclass;// defining the myclass in the form layer rather than in each buttons' own layer
+
+        public Form1()
+        {
+            InitializeComponent();
+            myclass = new Database();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        //button for viewing the data stored
+        //changes view order based on the value in "comboBoxOrderBy"
+        {
+            string orderBy = "";
+            switch (comboBoxOrderBy.SelectedItem.ToString())
+            {
+                case "Surname Alphabetical":
+                    orderBy = "ORDER BY LastName ASC";
+                    break;
+                case "Forename Alphabetical":
+                    orderBy = "ORDER BY FirstName ASC";
+                    break;
+                case "By ID":
+                    orderBy = "ORDER BY ClientID ASC";
+                    break;
+                default:
+                    orderBy = "";
+                    break;
+            }
+            
+            string query = "select * from Client " + orderBy;
+            D1.DataSource= myclass.Execute(query);            
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+        public class Database
+        {
+
+            public MySqlConnection con;
+            public string server;
+            public string database;
+            public string username;
+            public string password;
+
+            public  Database()
+            {
+                //setting up a variable to be used as the connection string as this is used every time a connection is made
+                string Connectionstring = "server=ysjcs.net; Database=tomchachula; Uid=tom.chachula; Pwd=JA5MFUU4;";
+                con = new MySqlConnection(Connectionstring);
+            }
+            public DataTable Execute(string query)
+            {
+                con.Open();
+                DataTable dt = new DataTable();
+                MySqlCommand cmd = new MySqlCommand(query, con);
+                //following line was used regularly for debugging, can be used to output any queries sent to the database so they can be checked for errors
+                //MessageBox.Show(query);
+                MySqlDataReader reader = cmd.ExecuteReader();
+                dt.Load(reader);
+                con.Close();
+                return dt;
+            }
+            //creating the insert function
+            public void insert(string query, MySqlParameter[] Parameters = null)
+            {
+                try
+                {
+                    con.Open();
+                    MySqlCommand command = new MySqlCommand(query, con);
+                    if (Parameters != null)
+                    {
+                        command.Parameters.AddRange(Parameters);
+                    }
+                    command.ExecuteNonQuery();
+                }
+                //added some error handling to prevent the code from crashing after any erroneous data entry
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Error during insert operation: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                finally
+                {
+                    con.Close();
+                }
+            }
+
+            //creating the update function. While they are very similar, they are kept seperate for code sanitisation
+            //this is useful for testing the two functions seperately
+            public void update(string query, MySqlParameter[] Parameters = null)
+            {
+                try
+                {
+                    con.Open();
+                    MySqlCommand command = new MySqlCommand(query, con);
+                    if (Parameters != null)
+                    {
+                        command.Parameters.AddRange(Parameters);
+                    }
+                    command.ExecuteNonQuery();
+                }
+                //same error handling message as in insert function
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Error during update operation: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                finally
+                {
+                    con.Close();
+                }
+            }
+        }
+
+        //creating the event sequence for when the insert button (button2) is clicked
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //first a blank insert query is created
+            string query = "insert into Client(ClientID, FirstName, LastName, Postcode, Phone, Email, Category) values(@ClientID, @FirstName, @LastName, @Postcode, @Phone, @Email, @Category)";
+            //then a parameter list is created with spaces for each column in the table
+            MySqlParameter[] param = new MySqlParameter[7];
+            //and they are all populated with the values in the relevant textboxes
+            param[0] = new MySqlParameter("@ClientID", MySqlDbType.Int32);
+            param[0].Value = textBox1.Text;
+            param[1] = new MySqlParameter("@FirstName", MySqlDbType.VarChar);
+            param[1].Value = textBox2.Text;
+            param[2] = new MySqlParameter("@LastName", MySqlDbType.VarChar);
+            param[2].Value = textBox3.Text;
+            param[3] = new MySqlParameter("@Postcode", MySqlDbType.VarChar);
+            param[3].Value = textBox4.Text;
+            param[4] = new MySqlParameter("@Phone", MySqlDbType.Int32);
+            param[4].Value = textBox5.Text;
+            param[5] = new MySqlParameter("@Email", MySqlDbType.VarChar);
+            param[5].Value = textBox6.Text;
+            param[6] = new MySqlParameter("@Category", MySqlDbType.Enum);
+            param[6].Value = comboBox1.Text;
+            //finally the query is sent to the insert function
+            myclass.insert(query, param);
+        }
+        
+        //the update event sequence is very similar
+        private void button4_Click(object sender, EventArgs e)
+        {
+            //but the blank query created uses the relevant keywords update, set, and where (to avoid the change being sent to every entry)
+            string query = "UPDATE Client SET FirstName = @FirstName, LastName = @LastName, Postcode = @Postcode, Phone = @Phone, Email = @Email, Category = @Category WHERE ClientID = @ClientID";
+            //parameter assignment is the same
+            MySqlParameter[] param = new MySqlParameter[7];
+            param[0] = new MySqlParameter("@ClientID", MySqlDbType.Int32);
+            param[0].Value = textBox1.Text;
+            param[1] = new MySqlParameter("@FirstName", MySqlDbType.VarChar);
+            param[1].Value = textBox2.Text;
+            param[2] = new MySqlParameter("@LastName", MySqlDbType.VarChar);
+            param[2].Value = textBox3.Text;
+            param[3] = new MySqlParameter("@Postcode", MySqlDbType.VarChar);
+            param[3].Value = textBox4.Text;
+            param[4] = new MySqlParameter("@Phone", MySqlDbType.Int32);
+            param[4].Value = textBox5.Text;
+            param[5] = new MySqlParameter("@Email", MySqlDbType.VarChar);
+            param[5].Value = textBox6.Text;
+            param[6] = new MySqlParameter("@Category", MySqlDbType.Enum);
+            param[6].Value = comboBox1.Text;
+            //finally the finished query is sent to the update function
+            myclass.update(query, param);
+        }
+        
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Populating the Order By comboBox
+            comboBoxOrderBy.Items.Add("By ID");
+            comboBoxOrderBy.Items.Add("Surname Alphabetical");
+            comboBoxOrderBy.Items.Add("Forename Alphabetical");
+            
+            //setting the default selection to ID
+            comboBoxOrderBy.SelectedIndex = 0;
+        }
+
+        private void comboBoxOrderBy_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+   
+
+}
